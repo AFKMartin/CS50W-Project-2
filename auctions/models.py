4dde72f9fc099bd366dcbf146bdc71b5b0b2c3e6
AFKMartin <javiermarace@protonmail.com>
2025-11-03 21:05:13 +0100

    models.py

--- auctions/models.py
+++ auctions/models.py
@@ -1,7 +1,36 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
 
 class User(AbstractUser):
     pass
 
-class 
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True) 
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="listings")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="listings")
+    is_active = models.BooleanField(default=True) 
+    creation_date = models.DateTimeField(auto_now_add=True)  
+
+
+class Bid(models.Model):
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} bid {self.amount} on {self.auction_listing}"
+                        
+
+class Comment(models.Model):
+    pass # TODO
