888b349baee76588861741d02b5d095fc1b715f7
AFKMartin <javiermarace@protonmail.com>
2025-11-22 07:46:05 +0100

    all implemented

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import AuctionListing, Category, User
+from .models import *
 
-admin.site.register(AuctionListing)
-admin.site.register(Category)
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Watchlist)
+admin.site.register(Notification)
